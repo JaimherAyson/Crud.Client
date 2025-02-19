c6bbc19f84cf392daa8f765e757f518df5f34964
Jaimher Ayson <aysonjam17@gmail.com>
2025-02-19 14:35:41 +0800

    Still fixing bugs

--- Crud.Client/Program.cs
+++ Crud.Client/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
+builder.Services.AddScoped<ItemService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
